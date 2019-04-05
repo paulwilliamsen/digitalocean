3a850ce545ec782e166e90bf52472e55cdf740ab
Paul Williamsen <pwilliamsen@me.com>
2019-04-04 17:37:08 -0700

    worked on files being setup for app

--- web/kickstarter/urls.py
+++ web/kickstarter/urls.py
@@ -1,4 +1,4 @@
-"""sheera_project URL Configuration
+"""kickstarter URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -14,10 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from .views import hello_view
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', hello_view, name="home")
+    path('', include('kickstarter_data.urls'))
 ]
--- web/sheera_project/views.py
+++
@@ -1,3 +0,0 @@
-from django.http import HttpResponse
-def hello_view(request):
-    return HttpResponse('I am NOT Sheera')
